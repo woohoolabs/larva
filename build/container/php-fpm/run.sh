4299e42970e271488634d275258845bc8b6b49d5
kocsis.mate <kocsismate@woohoolabs.com>
2017-01-28 09:18:23 +0100

    Added custom php.ini

--- build/container/php-fpm/run.sh
+++ build/container/php-fpm/run.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 set -e
 
+cp /code/build/container/php-fpm/custom-php.ini /usr/local/etc/php/conf.d/zz-custom-php.ini
+
 /code/build/container/php-fpm/schema.sh
 
 php-fpm
--- examples/index.php
+++ examples/index.php
@@ -1,8 +1,6 @@
 <?php
 declare(strict_types=1);
 
-error_reporting(E_ALL);
-ini_set("display_errors", "1");
 header('Content-Type: text/html; charset=utf-8');
 mb_internal_encoding("UTF-8");
 mb_http_output('UTF-8');
